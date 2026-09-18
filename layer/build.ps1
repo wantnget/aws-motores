98e89e7787222a5d65339205e4bce94255f788e4
FabCM00 <xzenzi259@gmail.com>
2026-09-17 22:03:49 -0500

    feat: pipeline ci/cd para lambda layer con docker, terraform y oidc

--- layer/build.ps1
+++ layer/build.ps1
@@ -1,4 +1,4 @@
-$ErrorActionPreference = 'Stop'
+﻿$ErrorActionPreference = 'Stop'
 $ScriptDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 Set-Location $ScriptDir
 
@@ -6,6 +6,6 @@ Write-Host '==> [1/2] Construyendo imagen Docker para Lambda Layer...' -Foregrou
 docker build -t lambda-layer-builder .
 
 Write-Host '==> [2/2] Extrayendo layer.zip compatible con Lambda...' -ForegroundColor Cyan
-docker run --rm -v "${ScriptDir}:/output" lambda-layer-builder
+docker run --rm --entrypoint /bin/bash -v "${ScriptDir}:/output" lambda-layer-builder -c "cd /opt && zip -r9 /output/layer.zip python"
 
 Write-Host "==> Layer empaquetada exitosamente en $ScriptDir\layer.zip" -ForegroundColor Green
--- layer/build.sh
+++ layer/build.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+﻿#!/usr/bin/env bash
 set -euo pipefail
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd)"
@@ -8,6 +8,6 @@ echo "==> [1/2] Construyendo imagen Docker para Lambda Layer..."
 docker build -t lambda-layer-builder .
 
 echo "==> [2/2] Extrayendo layer.zip compatible con Lambda..."
-docker run --rm -v "$DIR":/output lambda-layer-builder
+docker run --rm --entrypoint /bin/bash -v "$DIR":/output lambda-layer-builder -c "cd /opt && zip -r9 /output/layer.zip python"
 
 echo "==> Layer empaquetada exitosamente en $DIR/layer.zip"
